0b7fb053448bf5eaf275066fa5eedd9804e6d8a0
Alon Mor <alonmor@Alons-MacBook-Pro.local>
2022-01-07 23:18:15 -0500

    changed client in server

--- server/handlers/client.js
+++ server/handlers/client.js
@@ -1,17 +1,22 @@
-import ApolloClient from "apollo-boost";
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  fetch: fetch
+});
 
-export const createClient = (shop, accessToken) => {
-  return new ApolloClient({
-    uri: `https://${shop}/admin/api/2019-10/graphql.json`,
-    request: operation => {
-      operation.setContext({
-        headers: {
-          "X-Shopify-Access-Token": accessToken,
-          "User-Agent": `shopify-app-node ${
-            process.env.npm_package_version
-          } | Shopify App CLI`
-        }
-      });
-    }
-  });
-};
+// import ApolloClient from "apollo-boost";
+
+// export const createClient = (shop, accessToken) => {
+//   return new ApolloClient({
+//     uri: `https://${shop}/admin/api/2019-10/graphql.json`,
+//     request: operation => {
+//       operation.setContext({
+//         headers: {
+//           "X-Shopify-Access-Token": accessToken,
+//           "User-Agent": `shopify-app-node ${
+//             process.env.npm_package_version
+//           } | Shopify App CLI`
+//         }
+//       });
+//     }
+//   });
+// };
